289c2669c7b289ebd6a6253f7e0f19011a2f5f4e
ngoctai190497 <ngoctai190497@gmail.com>
2021-11-13 16:37:46 +0700

    done migration

---
+++ database/migrations/2021_11_13_084640_create_books_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('thumbnail');
+            $table->integer('price');
+            $table->integer('discount')->nullable();
+            $table->integer('amount');
+            $table->integer('view');
+            $table->longText('overview');
+            $table->integer('since');
+            $table->bigInteger('author_id');
+            // Lay author_id nay lien ket toi id trong bang authors
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+            // tao ra truong (category_id)
+            $table->bigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
